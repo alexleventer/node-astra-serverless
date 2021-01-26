ef20166dd8dc4f4ecf51be7a073f4d67412f6c9a
Alex Leventer <alexleventer@gmail.com>
2021-01-25 19:59:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Node Astra Serverless
+
+1. install the serverless cli: `npm install -g serverless`
+2. install node modules: `npm install`
---
+++ lib/astra.ts
@@ -0,0 +1,19 @@
+import { createClient } from '@astrajs/collections';
+
+const {
+  ASTRA_DB_ID,
+  ASTRA_DB_REGION,
+  ASTRA_DB_USERNAME,
+  ASTRA_DB_PASSWORD,
+  ASTRA_DB_KEYSPACE
+} = process.env;
+
+export const getCollection = async (collectionName) => {
+  const astraClient = await createClient({
+    astraDatabaseId: ASTRA_DB_ID,
+    astraDatabaseRegion: ASTRA_DB_REGION,
+    username: ASTRA_DB_USERNAME,
+    password: ASTRA_DB_PASSWORD,
+  });
+  return await astraClient.namespace(ASTRA_DB_KEYSPACE).collection(collectionName);
+};
